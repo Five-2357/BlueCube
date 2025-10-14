f0098c23c265b015d55c97ec9e9ccf8466f47795
Five2357 <186026883+Five-2357@users.noreply.github.com>
2025-10-14 20:45:33 +0400

    Update iframe.js

--- Site/stuff/iframe.js
+++ Site/stuff/iframe.js
@@ -1,3 +1,7 @@
-if (window.top !== window.self && !document.referrer.startsWith("https://bluecube.pages.dev")) {
-  window.top.location = window.location;
-}
+  if (window.top !== window.self && !document.referrer.startsWith("https://bluecube.pages.dev")) {
+    document.body.textContent = "NOT ALLOWED";
+
+    setTimeout(() => {
+      window.top.location = window.location; // Redirect after 3 sec
+    }, 3000);
+  }
